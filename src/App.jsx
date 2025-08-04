d24b6aac80bb3983898ebd0f84c180d34c698391
Asad <hellohello51981@gmail.com>
2025-08-04 11:40:02 +0500

    Typing Test Game - Build the Basic Structure f our Game

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
 
 const App = () => {
   return (
     <div>
-      
+      <div>
+        <h1>React Typing Game</h1>
+        <textarea className="border" />
+        <h4 >Remaining Time: </h4>
+        <button>Start</button>
+        <h1>Total Words</h1>
+      </div>
     </div>
   )
 }
