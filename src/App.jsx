85e1b08fb49667ad8c74580bb10a92e7d9ea4a7c
Asad <hellohello51981@gmail.com>
2025-08-04 11:42:33 +0500

    Typing Test Game - Using hooks, track the State of the text of textArea

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react"
 
 const App = () => {
+  
+  const[text,setTextArea] = useState({
+      typingText:""
+  })
+
+  const handleChnage = (e)=>{
+    setTextArea({
+      typingText: e.target.value
+    })
+  }
+
   return (
-    
-
-
-
     <div>
       <div className="font-typing text-typing-text bg-black text-center p-4">
         <h1 className="text-2xl font-bold m-2">React Typing Game</h1>
-        <textarea className="text-black border outline-none bg-typing-text w-11/12 resize h-56 m-2 p-2" />
+        <textarea 
+        value={text.typingText}  
+        name="typingText"
+        className="text-black border outline-none bg-typing-text w-11/12 resize h-56 m-2 p-2" 
+        onChange={handleChnage}
+        />
         <h4 className="font-bold m-2">Remaining Time: </h4>
         <button className="bg-typing-text text-black py-2.5 px-3.5 block mx-auto">Start</button>
         <h1 className="text-2xl font-bold m-2">Total Words</h1>
