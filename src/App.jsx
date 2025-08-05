e105ca1dd2336cf05e68155f89e923bc231cb679
Asad <hellohello51981@gmail.com>
2025-08-05 12:03:27 +0500

    Typing Test Game - create a custom to store all the hooks and other business logics

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,15 @@
 import { useState,useEffect, useRef } from "react"
 
 const App = () => {
-  const START_TIME = 15
   
-  const[text,setTextArea] = useState({
-      typingText:""
-  })
-
-  const[timeRemaining,setRemainingTime] = useState(START_TIME)
-  const[IsTimeRunning,setTimeRunning] = useState(false)
-  const[word,setWordCount] = useState(0)
-  const textRef = useRef(null)
-
-  const handleChnage = (e)=>{
-    setTextArea({
-      typingText: e.target.value
-    })
-  }
-
-  useEffect(()=>{
-    if(IsTimeRunning === true && timeRemaining > 0){
-        setTimeout(()=>{
-          setRemainingTime(time => time-1)
-        },1000) 
-      }else{
-        endGame()
-        
-      }
-      
-  },[timeRemaining,IsTimeRunning])
-
-  const startGame = ()=>{
-    setTimeRunning(true)
-    setRemainingTime(START_TIME)
-    setTextArea({
-      typingText: ""
-    })
-    textRef.current.disabled = false
-    textRef.current.focus()
-  }
-
-  const endGame = ()=>{
-    setTimeRunning(false)
-    setWordCount(calculateWordCount(text.typingText))
-  }
-
-
-  const calculateWordCount = (text)=>{
-    const wordArr = text.trim().split(" ")
-    const filterWord = wordArr.filter(word => word !== "")
-    return filterWord.length
-  }
 
   return (
     <div>
       <div className="font-typing text-typing-text bg-black text-center p-4">
         <h1 className="text-2xl font-bold m-2">React Typing Game</h1>
         <textarea 
-        value={text.typingText}  
-        name="typingText"
+        value={text}  
+        name="text"
         className={`border outline-none w-11/12 resize h-56 m-2 p-2 ${!IsTimeRunning ? "bg-disable-cl":"bg-typing-text text-black"}` }
         onChange={handleChnage}
         disabled={!IsTimeRunning}
---
+++ src/hooks/customhook.jsx
@@ -0,0 +1,51 @@
+function useCustomHook(){
+    const START_TIME = 15
+  
+  const[text,setTextArea] = useState("")
+
+  const[timeRemaining,setRemainingTime] = useState(START_TIME)
+  const[IsTimeRunning,setTimeRunning] = useState(false)
+  const[word,setWordCount] = useState(0)
+  const textRef = useRef(null)
+
+  const handleChnage = (e)=>{
+    setTextArea(e.target.value)
+  }
+
+  useEffect(()=>{
+    if(IsTimeRunning === true && timeRemaining > 0){
+        setTimeout(()=>{
+          setRemainingTime(time => time-1)
+        },1000) 
+      }else{
+        endGame()
+        
+      }
+      
+  },[timeRemaining,IsTimeRunning])
+
+  const startGame = ()=>{
+    setTimeRunning(true)
+    setRemainingTime(START_TIME)
+    setTextArea({
+      typingText: ""
+    })
+    textRef.current.disabled = false
+    textRef.current.focus()
+  }
+
+  const endGame = ()=>{
+    setTimeRunning(false)
+    setWordCount(calculateWordCount(text))
+  }
+
+
+  const calculateWordCount = (text)=>{
+    const wordArr = text.trim().split(" ")
+    const filterWord = wordArr.filter(word => word !== "")
+    return filterWord.length
+  }
+  return {text,IsTimeRunning,timeRemaining,handleChnage,textRef,startGame,word}
+}
+
+export default useCustomHook
